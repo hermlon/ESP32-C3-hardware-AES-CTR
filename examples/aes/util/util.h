6ca58951b5674cfa60bea5d5cbd5df261b282b61
Gustav Lahmann <gustav.lahmann@uni-jena.de>
2022-07-04 12:15:40 +0200

    Added benchmark from paper

---
+++ examples/aes/util/util.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+uint32_t get_cycles();
+void init_cycles();
+
+#endif
---
+++ examples/aes/util/util.s
@@ -0,0 +1,19 @@
+.text
+
+.globl init_cycles
+.align 2
+init_cycles:
+    /* write 0 to value register */
+    csrrwi x0, 0x7E2, 0
+    /* count clock cycles */
+    csrrwi x0, 0x7E0, 1 
+    /* start counting, halt on max value */
+    csrrwi x0, 0x7E0, 3 
+.size init_cycles,.-init_cycles
+
+.globl get_cycles
+.align 2
+get_cycles:
+    csrr a0, 0x7E2
+    ret
+.size get_cycles,.-get_cycles
