d83cbde752631a2ed41e2936db5bf83c7a8c379d
Gustav Lahmann <gustav.lahmann@uni-jena.de>
2022-07-10 01:42:08 +0200

    AES works correctly

--- examples/aes/aes_hw/aes_hw.c
+++ examples/aes/aes_hw/aes_hw.c
@@ -4,78 +4,67 @@
 
 #define GDMA_CHN 0
 
-/* transmit to AES */
-
-static lldesc_t block_in_desc_2 = {
-    .size = 4096-4,
-    .length = 4096-4,
-    .suc_eof = 1,
-    .owner = 1,
-    .qe = NULL
-};
+/* receive from AES */
 static lldesc_t block_in_desc = {
     .size = 4096-4,
-    .length = 4096-4,
+    .length = 0,
     .suc_eof = 0,
     .owner = 1,
-    .qe = &block_in_desc_2
+    .qe = NULL
 };
 
-/* receive from AES */
-
-static lldesc_t block_out_desc_2 = {
-    .size = 4096-16,
+/* transmit to AES */
+static lldesc_t block_out_desc = {
+    .size = 4096-4,
     .length = 0,
     .suc_eof = 1,
     .owner = 1,
     .qe = NULL
 };
-static lldesc_t block_out_desc = {
-    .size = 4096-16,
-    .length = 0,
-    .suc_eof = 0,
-    .owner = 1,
-    .qe = &block_out_desc_2
-};
 
-void print_status_regs() {
+void print_status_regs(int num) {
     return;
     uint8_t n = GDMA_CHN;
-    printf("78: %p\n", (void*)REG_READ(C3_GDMA + 0x78 + (192*n)));
-    printf("84: %p\n", (void*)REG_READ(C3_GDMA + 0x84 + (192*n)));
-    printf("88: %p\n", (void*)REG_READ(C3_GDMA + 0x88 + (192*n)));
-    printf("8c: %p\n", (void*)REG_READ(C3_GDMA + 0x8c + (192*n)));
-    printf("90: %p\n", (void*)REG_READ(C3_GDMA + 0x90 + (192*n)));
-    printf("94: %p\n", (void*)REG_READ(C3_GDMA + 0x94 + (192*n)));
-    printf("98: %p\n", (void*)REG_READ(C3_GDMA + 0x98 + (192*n)));
-    printf("d8: %p\n", (void*)REG_READ(C3_GDMA + 0xd8 + (192*n)));
-    printf("e4: %p\n", (void*)REG_READ(C3_GDMA + 0xe4 + (192*n)));
-    printf("e8: %p\n", (void*)REG_READ(C3_GDMA + 0xe8 + (192*n)));
-    printf("ec: %p\n", (void*)REG_READ(C3_GDMA + 0xec + (192*n)));
-    printf("f0: %p\n", (void*)REG_READ(C3_GDMA + 0xf0 + (192*n)));
-    printf("f4: %p\n", (void*)REG_READ(C3_GDMA + 0xf4 + (192*n)));
-    printf("f8: %p\n", (void*)REG_READ(C3_GDMA + 0xf8 + (192*n)));
+    printf("(%d)\n", num);
+    
+    uint32_t interrupt = REG_READ(C3_GDMA + 0x04 + (16*n));
+    printf("INT: %p\n", (void*)interrupt);
+    printf("len: %d\n", block_in_desc.length);
+    printf("eof: %d\n", block_in_desc.suc_eof);
+    printf("own: %d\n", block_in_desc.owner);
+
+    uint32_t r78 = REG_READ(C3_GDMA + 0x78 + (192*n));
+    printf("RX fifo      78: %p ", (void*)r78);
+    printf("INFIFO FULL: %ld, ", r78 & 1);
+    printf("INFIFO EMPTY: %ld ", (r78 & 2) >> 1);
+    printf("INFIFO CNT: %ld\n", (r78 & 0b11111100) >> 2);
+
+    printf("RX status    84: %p\n", (void*)REG_READ(C3_GDMA + 0x84 + (192*n)));
+    printf("RX last addr 94: %p\n", (void*)REG_READ(C3_GDMA + 0x94 + (192*n)));
+
+    uint32_t rd8 = REG_READ(C3_GDMA + 0xd8 + (192*n));
+    printf("TX fifo      78: %p ", (void*)rd8);
+    printf("OUTFIFO FULL: %ld, ", rd8 & 1);
+    printf("OUTFIFO EMPTY: %ld ", (rd8 & 2) >> 1);
+    printf("OUTFIFO CNT: %ld\n", (rd8 & 0b11111100) >> 2);
+
+    printf("TX status    e4: %p\n", (void*)REG_READ(C3_GDMA + 0xe4 + (192*n)));
+    printf("TX eof addr  e8: %p\n", (void*)REG_READ(C3_GDMA + 0xe8 + (192*n)));
     printf("--------------------------------%s\n", "");
 }
 
 
 /* expects 256 blocks, so 4096 bytes */
-void aes_hw_encrypt_ctr(const uint8_t* key, const uint8_t* iv, const uint8_t* in, uint8_t* out, uint32_t length) {
-    block_in_desc.buf = in;
-    block_in_desc.length = length & 0xfff;
-    //block_in_desc_2.buf = in;
-    block_out_desc.buf = out;
-    //block_out_desc.length = length & 0xfff;
+void aes_hw_encrypt_ctr(const uint8_t* key, const uint8_t* iv, const uint8_t* in, uint8_t* out, uint32_t blocks) {
+    uint32_t length = blocks * 16;
 
-    block_in_desc_2.buf = in;
-    block_out_desc_2.buf = out + INPUT_LENGTH;
-    
-    /*
-    printf("in: %p\n", in);
-    printf("out: %p\n", out);
-    printf("inlink: %p\n", (void*)&block_in_desc);
+    block_in_desc.buf = out;
+    block_out_desc.buf = in;
+
+    block_out_desc.length = length & 0xfff;
+
     printf("outlink: %p\n", (void*)&block_out_desc);
-    */
+    printf("inlink: %p\n", (void*)&block_in_desc);
 
     /* periph_ll_enable_clk_clear_rst https://github.com/espressif/esp-idf/blob/16a4ee7c36a848ca155791677ce011f3ca75c519/components/hal/esp32c3/include/hal/clk_gate_ll.h#L202 */
     // enable AES clock
@@ -90,7 +79,7 @@ void aes_hw_encrypt_ctr(const uint8_t* key, const uint8_t* iv, const uint8_t* in
     uint32_t key_word;
     for(int i = 0; i < 4; i++) {
         memcpy(&key_word, key + i*4, 4);
-        REG_WRITE(((uint32_t*)AES_KEY_BASE) + i*4, key_word);
+        REG_WRITE(((uint32_t*)AES_KEY_BASE) + i, key_word);
     }
 
     // set block mode to CTR
@@ -100,12 +89,10 @@ void aes_hw_encrypt_ctr(const uint8_t* key, const uint8_t* iv, const uint8_t* in
     REG_WRITE(AES_INC_SEL_REG, AES_INC_32);
 
     // copy IV / Initial Counter Block
-    memcpy((uint8_t*)AES_IV_BASE, iv, 16);
-    
     uint32_t iv_word;
-    for(int i = 0; i < 16; i++) {
+    for(int i = 0; i < 4; i++) {
         memcpy(&iv_word, iv + i*4, 4);
-        REG_WRITE(((uint32_t*)AES_IV_BASE) + i*4, iv_word);
+        REG_WRITE(((uint32_t*)AES_IV_BASE) + i, iv_word);
     }
     
     // disable interrupts
@@ -114,31 +101,36 @@ void aes_hw_encrypt_ctr(const uint8_t* key, const uint8_t* iv, const uint8_t* in
     /* 1. connect DMA with AES */
     esp_aes_dma_start(&block_in_desc, &block_out_desc);
     
+    print_status_regs(6);
     /* 2. Initialize the AES accelerator-related registers */
     REG_WRITE(AES_DMA_ENABLE_REG, 1);
     
+    print_status_regs(7);
     // set number of blocks
-    REG_WRITE(AES_BLOCK_NUM_REG, NUM_BLOCKS);
+    REG_WRITE(AES_BLOCK_NUM_REG, blocks);
     
     /* 3. start operation */
     REG_WRITE(AES_TRIGGER_REG, 1);
     
-    print_status_regs();
+    print_status_regs(8);
     /* 4. wait until AES done */
-    //while(REG_READ(AES_STATE_REG) != AES_STATE_DONE) {
     //    printf("%ld\n", REG_READ(AES_STATE_REG));
     //printf("INT: %ld\n", REG_READ(DMA_INT_ST_CH0_REG));
     //printf("INT: %p\n", (void*)REG_READ(DMA_INT_ST_CH0_REG));
-    //}
-    while(block_out_desc.owner != 0) {
+    while(REG_READ(AES_STATE_REG) != AES_STATE_DONE) {
+        //print_status_regs(9);
     }
-    //printf("done\n%s", "");
-
+    
+    while(block_in_desc.owner != 0) {}
+    while(block_in_desc.suc_eof == 0) {}
+    printf("done\n%s", "");
+    print_status_regs(9);
 }
 
 void esp_aes_dma_start(const lldesc_t *input, const lldesc_t *output) {
-    //REG_WRITE((C3_GDMA + 0x0008), 0xffff);
-    print_status_regs();
+    // enable all interrupts
+    REG_WRITE((C3_GDMA + 0x0008), 0xffff);
+    print_status_regs(1);
 
     // 1. Set GDMA_OUT_RST_CH0 first to 1 and then to 0, to reset the state machine of GDMA’s transmit channel and FIFO pointer
     // in and out
@@ -147,15 +139,15 @@ void esp_aes_dma_start(const lldesc_t *input, const lldesc_t *output) {
     CLEAR_REG_MASK(DMA_IN_CONF0_CHn_REG(GDMA_CHN), GDMA_IN_RST_CHn);
     CLEAR_REG_MASK(DMA_OUT_CONF0_CHn_REG(GDMA_CHN), GDMA_OUT_RST_CHn);
 
-    print_status_regs();
+    print_status_regs(2);
 
     // 2. Load an outlink, and configure GDMA_OUTLINK_ADDR_CHn with address of the first transmit descriptor
     // in
-    REG_SET_BITS(DMA_IN_LINK_CHn_REG(GDMA_CHN), output, DMA_INLINK_ADDR_CHn);
+    REG_SET_BITS(DMA_IN_LINK_CHn_REG(GDMA_CHN), input, DMA_INLINK_ADDR_CHn);
     // out
-    REG_SET_BITS(DMA_OUT_LINK_CHn_REG(GDMA_CHN), input, DMA_OUTLINK_ADDR_CHn);
+    REG_SET_BITS(DMA_OUT_LINK_CHn_REG(GDMA_CHN), output, DMA_OUTLINK_ADDR_CHn);
 
-    print_status_regs();
+    print_status_regs(3);
 
     // 3. Configure GDMA_PERI_OUT_SEL_CHn with the value corresponding to the peripheral to be connected, as shown in Table 2-1
     // in
@@ -163,7 +155,7 @@ void esp_aes_dma_start(const lldesc_t *input, const lldesc_t *output) {
     // out
     REG_WRITE(DMA_OUT_PERI_SEL_CHn_REG(GDMA_CHN), PERI_SEL_AES);
 
-    print_status_regs();
+    print_status_regs(4);
 
     // 4. Set GDMA_OUTLINK_START_CHn to enable GDMA’s transmit channel for data transfer
     // in
@@ -171,5 +163,5 @@ void esp_aes_dma_start(const lldesc_t *input, const lldesc_t *output) {
     // out
     SET_REG_MASK(DMA_OUT_LINK_CHn_REG(GDMA_CHN), DMA_OUTLINK_START_CHn);
 
-    print_status_regs();
+    print_status_regs(5);
 }
